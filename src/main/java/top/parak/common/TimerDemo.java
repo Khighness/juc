6fc3e495311811edfb4bd13cf246bb6391ca2c46
Khighness <1823676372@qq.com>
2021-05-02 00:16:01 +0800

    🌠 add TimerDemo.java

---
+++ src/main/java/top/parak/common/TimerDemo.java
@@ -0,0 +1,39 @@
+package top.parak.common;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author KHighness
+ * @since 2021-05-01
+ */
+
+@Slf4j(topic = "Timer")
+public class TimerDemo {
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        TimerTask task1 = new TimerTask() {
+            @Override
+            public void run() {
+                log.debug("task1");
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        TimerTask task2 = new TimerTask() {
+            @Override
+            public void run() {
+                log.debug("task2");
+            }
+        };
+        log.debug("start...");
+        timer.schedule(task1, 1000);
+        timer.schedule(task2, 1000);
+    }
+}
