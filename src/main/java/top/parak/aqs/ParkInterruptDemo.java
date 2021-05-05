605878305d2ca5caf4ed4e708ac516625d8ee714
Khighness <1823676372@qq.com>
2021-05-05 12:19:00 +0800

    🌠 add ParkInterruptDemo.java

---
+++ src/main/java/top/parak/aqs/ParkInterruptDemo.java
@@ -0,0 +1,25 @@
+package top.parak.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @author KHighness
+ * @since 2021-05-03
+ */
+
+@Slf4j(topic = "ParkInterrupt")
+public class ParkInterruptDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(() -> {
+            log.debug("1");
+            LockSupport.park();
+            log.debug("2");
+        }, "t");
+        t.start();
+        TimeUnit.SECONDS.sleep(1);
+        t.interrupt();
+    }
+}
